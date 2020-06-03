0bfb83e2a7e6d5ae7543c25ba00dee3123aa3311
Lucas <61209498+CompileException@users.noreply.github.com>
2020-06-03 21:42:24 +0200

    PartySystem
    bug fixes

--- src/main/java/de/teamsoul/party/main/PartySystem.java
+++ src/main/java/de/teamsoul/party/main/PartySystem.java
@@ -7,11 +7,13 @@ package de.teamsoul.party.main;
 
 import de.teamsoul.party.config.Configuration;
 import de.teamsoul.party.listener.ServerSwitch;
+import de.teamsoul.party.mysql.MySQL;
 import de.teamsoul.party.utils.PartyManager;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Plugin;
 
+import java.awt.*;
 import java.io.File;
 import java.util.HashMap;
 
@@ -27,16 +29,18 @@ public class PartySystem extends Plugin {
     private static PartySystem instance;
     public HashMap<ProxiedPlayer, PartyManager> manager;
 
+    public MySQL mySQL = new MySQL(hostname, port, database, username, password);
+
     @Override
     public void onEnable() {
         System.out.println("[Party] System gestartet!");
         System.out.println("Version: " + getDescription().getVersion());
+        loadconfiguration();
 
         try {
             register();
             fetchingData();
-            loadconfiguration();
-
+            mysqlconnect();
             new File("./plugins/Party/").mkdir();
 
         } catch (Exception e) {
@@ -49,6 +53,9 @@ public class PartySystem extends Plugin {
     @Override
     public void onDisable() {
         System.out.println("[Party] System gestoppt!");
+
+        mysqldisconnect();
+
 
     }
 
@@ -81,6 +88,21 @@ public class PartySystem extends Plugin {
             username = configuration.getString("mysql.User");
             configuration.saveAsConfig("./plugins/Party/");
         }
+    }
+
+    public void mysqlconnect() {
+        try {
+            mySQL.openConnection();
+            System.out.println(SystemColor.GREEN + "MySQL connected!");
+        } catch (Exception e) {
+            System.out.println(SystemColor.red + "Fehler beim verbinden der MySQL!");
+            e.printStackTrace(); }
+    }
+
+    public void mysqldisconnect() {
+        try {
+            mySQL.closeConnection();
+        } catch (Exception e) { e.printStackTrace(); }
     }
 
     public static PartySystem getInstance() {
